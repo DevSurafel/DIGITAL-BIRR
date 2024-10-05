0dd5734e9dc69e66fd43f4dee04b6335bd9c779f
Surafel Diriba <119774330+DevSurafel@users.noreply.github.com>
2024-10-05 17:50:32 +0530

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,33 @@
+import plutos from '../images/plutotap.svg'
+import bronze from './bronze.webp';
+import Silver from '/silver.webp';
+import Silver from '/silver.webp';
+import gold from '/gold.webp'
+import diamond from '/diamond.webp'
+import platinum from '/platinum.webp';
+import master from '/master.webp';
+import grandmaster from '/grandmaster.webp'
+import iron from '/iron.webp';
+import challenger from '/challenger.webp';
+import tapcoin from '../images/tapme1.webp'
+import lihgt from '../images/lihgt.webp'
+// import plutos from '../images/'
+// import plutos from '../images/'
+// import plutos from '../images/'
+// import plutos from '../images/'
+// import plutos from '../images/'
+
+export {
+    plutos,
+    iron,
+    bronze,
+    Silver,
+    challenger,
+    gold,
+    diamond,
+    platinum,
+    master,
+    tapcoin,
+    lihgt,
+    grandmaster,
+}
