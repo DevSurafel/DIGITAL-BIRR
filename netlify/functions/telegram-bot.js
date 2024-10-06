fa0280bcb96237a19e8a162d1418387ff9b2ee12
Surafel Diriba <119774330+DevSurafel@users.noreply.github.com>
2024-10-06 13:14:52 +0530

    Update telegram-bot.js

--- netlify/functions/telegram-bot.js
+++ netlify/functions/telegram-bot.js
@@ -14,16 +14,12 @@ bot.start((ctx) => {
   const user = ctx.message.from;
   const userName = user.username ? `@${user.username}` : user.first_name;
 
-  return ctx.replyWithMarkdown(`*Hey ${userName}, Welcome to Krypto!! Tap into the future of finance! Participate in challenges and watch your token balance soar!*
-Tap on the coin and see tokens rise!.
-
-*Krypto* is a revolutionary decentralized finance platform where users can earn rewards by leveraging our innovative features. The majority of Krypto token distribution will occur among our active community members.
-
-Got friends, family, or colleagues? Invite them to join the fun! More friends, more KRYPTO tokens.
-Share the excitement and start building your financial future today!`, {
+  return ctx.replyWithMarkdown(`*Hey ${userName}, Welcome to [**$BIRR**](https://t.me/+p9ThUnIaaV0wYzZk)!* 
+Tap into the future of finance! \n\n
+Start building your financial future today!`, {
     reply_markup: {
       inline_keyboard: [
-        [{ text: "👋 Start now!", web_app: { url: urlSent } }],
+        [{ text: "Start now!", web_app: { url: urlSent } }],
         [{ text: "Join our Community", url: community_link }]
       ]
     }
