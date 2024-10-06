dbfd515c799f03d8e418189c108dee248dd52a01
Surafel Diriba <119774330+DevSurafel@users.noreply.github.com>
2024-10-06 15:21:51 +0530

    Update telegram-bot.js

--- netlify/functions/telegram-bot.js
+++ netlify/functions/telegram-bot.js
@@ -14,7 +14,7 @@ bot.start((ctx) => {
   const user = ctx.message.from;
   const userName = user.username ? `@${user.username}` : user.first_name;
 
-  return ctx.replyWithMarkdown(  `*Hey ${userName}, Welcome to [$BIRR](${urlSent}) 
+  return ctx.replyWithMarkdown(  `*Hey ${userName}, Welcome to [$BIRR]({urlSent}) 
 Tap into the future of finance! \n\n
 Start building your financial future today!`,  {
     reply_markup: {
