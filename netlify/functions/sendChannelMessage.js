02cdcc981c3073d0c1b37db6f6fb65ec2cd73eea
Surafel Diriba <119774330+DevSurafel@users.noreply.github.com>
2024-10-09 14:00:38 +0530

    Update sendChannelMessage.js

--- netlify/functions/sendChannelMessage.js
+++ netlify/functions/sendChannelMessage.js
@@ -1,17 +1,26 @@
-const { broadcastMessage } = require('../src/msg/channelMessageSender.js');
+const { Telegraf } = require("telegraf");
 
-exports.handler = async function (event, context) {
-  // Call the function to send the message
+// Replace with your bot token
+const bot = new Telegraf(process.env.REACT_APP_BOT_TOKEN);
+
+// Replace with your channel ID (e.g., @your_channel_name or channel ID)
+const channelId = "-1001379581156";
+
+// Message you want to send
+const message = "Hello, this is a test message to the channel!";
+
+exports.handler = async (event, context) => {
   try {
-    broadcastMessage();
+    await bot.telegram.sendMessage(channelId, message, { parse_mode: 'Markdown' });
     return {
       statusCode: 200,
-      body: "Message sent successfully!"
+      body: JSON.stringify({ message: "Message sent successfully!" }),
     };
   } catch (error) {
+    console.error("Error sending message:", error);
     return {
       statusCode: 500,
-      body: `Error sending message: ${error.message}`
+      body: JSON.stringify({ error: "Failed to send message" }),
     };
   }
 };
