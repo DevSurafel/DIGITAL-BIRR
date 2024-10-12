0f72d497864ab23db24382ec850368450a621bda
Surafel Diriba <119774330+DevSurafel@users.noreply.github.com>
2024-10-12 13:08:34 +0530

    Update Ref.js

--- src/pages/Ref.js
+++ src/pages/Ref.js
@@ -113,7 +113,7 @@ const Ref = () => {
       }));
     };
 
-    const calculateUserRank = (users) => {
+ const calculateUserRank = (users) => {
       if (!Array.isArray(users) || users.length === 0) {
         setUserRank("Not Ranked");
         return;
@@ -269,24 +269,24 @@ const Ref = () => {
             </div>
 
             <div className="!mt-[204px] w-full h-[60vh] flex flex-col overflow-y-auto">
-              <div
-                className={`${activeIndex === 1 ? "flex" : "hidden"} alltaskscontainer flex-col w-full space-y-2`}
-              >
-                <div className="w-full flex justify-between items-center rounded-lg">
-                  <div className="flex items-center space-x-4">
-                    <div className="flex flex-col w-full">
-                      <p className="text-white font-bold">
-                        {totalUsers} Holders
-                      </p>
-                    </div>
+            <div
+              className={`${activeIndex === 1 ? "flex" : "hidden"} alltaskscontainer flex-col w-full space-y-2`}
+            >
+              <div className="w-full flex justify-between items-center rounded-lg">
+                <div className="flex items-center space-x-4">
+                  <div className="flex flex-col w-full">
+                    <p className="text-white font-bold">
+                      {totalUsers} Holders
+                    </p>
                   </div>
-                  <div className="flex items-center space-x-4">  {leaderboardData.map((item) => (
-                    <p key={item.rank} className="text-white font-bold">Your Rank: #{item.rocks}</p>
-                  </div>
-                  <div className="flex items-center space-x-4">
-                    <p className="font-bold">Leagues</p>
-                  </div>
-                </div>)
+                </div>
+                <div className="flex items-center space-x-4">
+                  <p className="text-white font-bold">Your Rank: #{userRank}</p>
+                </div>
+                <div className="flex items-center space-x-4">
+                  <p className="font-bold">Leagues</p>
+                </div>
+              </div>
 
                 <div className="space-y-2">
                   {leaderboardData.map((item) => (
