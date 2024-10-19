1891ab7b53d609035c8c40d7855da89f31162cf2
Surafel Diriba <119774330+DevSurafel@users.noreply.github.com>
2024-10-19 13:35:02 +0530

    Update TaskTelegram.js

--- src/Components/Task/TaskTelegram.js
+++ src/Components/Task/TaskTelegram.js
@@ -67,7 +67,7 @@ const TaskTelegram = ({ showModal, setShowModal }) => {
 
 
   const handleTaskLinkClick = () => {
-    window.open("https://t.me/+BJZMDleqN0ljZThl");
+    window.open("https://t.me/+p9ThUnIaaV0wYzZk");
 
     setTimeout(() => {
       setShowTaskButton(false);
@@ -84,7 +84,7 @@ const TaskTelegram = ({ showModal, setShowModal }) => {
     }
 
     const response = await fetch(
-      `https://api.telegram.org/bot7855740816:AAFNYuqct3l3uSG18uW_3IMPXKuSHw5LC5k/getChatMember?chat_id=-1002157796678&user_id=${id}`
+      `https://api.telegram.org/bot${process.env.REACT_APP_TELEGRAM_BOT_TOKEN}/getChatMember?chat_id=-1001379581156&user_id=${id}`
     );
     const data = await response.json();
 
