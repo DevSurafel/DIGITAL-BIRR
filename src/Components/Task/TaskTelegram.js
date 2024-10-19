3a9d4b4918aaddaf0a897c5128c2531875b506d5
Surafel Diriba <119774330+DevSurafel@users.noreply.github.com>
2024-10-19 14:33:43 +0530

    Update TaskTelegram.js

--- src/Components/Task/TaskTelegram.js
+++ src/Components/Task/TaskTelegram.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { db } from "../../firebase";
+import { db } from "../../firebase"; // Adjust the import according to your file structure.
 import {
   collection,
   getDoc,
@@ -24,6 +24,7 @@ const TaskTelegram = ({ showModal, setShowModal }) => {
   const taskID = "task_tele_1";
   const [openComplete, setOpenComplete] = useState(false);
 
+  // Effect for handling the Telegram back button
   useEffect(() => {
     const handleBackButtonClick = () => {
       setShowModal(false);
@@ -43,6 +44,7 @@ const TaskTelegram = ({ showModal, setShowModal }) => {
     };
   }, [showModal, setShowModal]);
 
+  // Check task completion status when id changes
   useEffect(() => {
     if (id) {
       checkTaskCompletion(id, taskID).then((completed) => {
@@ -75,7 +77,7 @@ const TaskTelegram = ({ showModal, setShowModal }) => {
     setCounter(15);
     const newIntervalId = setInterval(() => {
       setCounter((prevCounter) => {
-        if (prevCounter === 1) {
+        if (prevCounter <= 1) {
           clearInterval(newIntervalId);
           setShowCheckButton(false);
           setShowDoneButton(true);
@@ -89,37 +91,34 @@ const TaskTelegram = ({ showModal, setShowModal }) => {
     setIntervalId(newIntervalId);
   };
 
-  const checkTaskCompletion = async (id, taskId) => {
+  const checkTaskCompletion = async (userId, taskId) => {
     try {
-      const userTaskDocRef = doc(db, "userTasks", `${id}_${taskId}`);
+      const userTaskDocRef = doc(db, "userTasks", `${userId}_${taskId}`);
       const docSnap = await getDoc(userTaskDocRef);
       return docSnap.exists() ? docSnap.data().completed : false;
-    } catch (e) {
-      console.error("Error checking task completion: ", e);
+    } catch (error) {
+      console.error("Error checking task completion: ", error);
       return false;
     }
   };
 
-  const saveTaskCompletionToFirestore = async (id, taskId, isCompleted) => {
+  const saveTaskCompletionToFirestore = async (userId, taskId, isCompleted) => {
     try {
-      const userTaskDocRef = doc(db, "userTasks", `${id}_${taskId}`);
-      await setDoc(
-        userTaskDocRef,
-        { userId: id, taskId: taskId, completed: isCompleted },
-        { merge: true }
-      );
-    } catch (e) {
-      console.error("Error saving task completion status: ", e);
+      const userTaskDocRef = doc(db, "userTasks", `${userId}_${taskId}`);
+      await setDoc(userTaskDocRef, { userId, taskId, completed: isCompleted }, { merge: true });
+    } catch (error) {
+      console.error("Error saving task completion status: ", error);
     }
   };
 
-  const updateUserCountInFirestore = async (id, newBalance) => {
+  const updateUserCountInFirestore = async (userId, newBalance) => {
     try {
       const userRef = collection(db, "telegramUsers");
       const querySnapshot = await getDocs(userRef);
       let userDocId = null;
+      
       querySnapshot.forEach((doc) => {
-        if (doc.data().userId === id) {
+        if (doc.data().userId === userId) {
           userDocId = doc.id;
         }
       });
@@ -130,8 +129,8 @@ const TaskTelegram = ({ showModal, setShowModal }) => {
       } else {
         console.error("User document not found.");
       }
-    } catch (e) {
-      console.error("Error updating user count in Firestore: ", e);
+    } catch (error) {
+      console.error("Error updating user count in Firestore: ", error);
     }
   };
 
@@ -166,48 +165,37 @@ const TaskTelegram = ({ showModal, setShowModal }) => {
     <>
       {showModal && (
         <div className="fixed z-50 left-0 right-0 top-0 bottom-0 flex justify-center taskbg px-[16px] h-full">
-          <div className="w-full flex flex-col items-center justify-start">
-            <div className="flex justify-start w-full py-2"></div>
+          <div className="w-full flex flex-col items-center">
             <div className="flex flex-col w-full">
               <h1 className="text-[20px] font-semibold">Join Our Telegram Channel</h1>
               <p className="text-[#9a96a6] text-[16px] font-medium pt-1 pb-10">
-                We regularly share valuable content on our channel. Join us there and get rewarded
+                We regularly share valuable content on our channel. Join us there and get rewarded!
               </p>
 
               <p className="w-full text-center text-[14px] font-semibold text-[#49ee49] pb-4">
                 {taskCompleted ? "Task is Completed" : ""}
               </p>
+
+              {/* Reward Information */}
               <div className="bg-cards rounded-[10px] p-[14px] flex justify-between items-center">
                 <div className="flex items-center flex-1 space-x-2">
-                  <div className="">
-                    <img src={require('../../images/coinsmall.png')} className="w-[50px]" alt="Coin Icon" />
-                  </div>
+                  <img src={require('../../images/coinsmall.png')} className="w-[50px]" alt="Coin Icon" />
                   <div className="flex flex-col space-y-1">
                     <span className="font-semibold">Reward</span>
-                    <div className="flex items-center">
-                      <span className="font-medium">50 000</span>
-                    </div>
+                    <span className="font-medium">50 000</span>
                   </div>
                 </div>
               </div>
-              <h1 className="text-[20px] font-semibold pt-6 pb-4 px-2">
-                Your Tasks
-              </h1>
+
+              <h1 className="text-[20px] font-semibold pt-6 pb-4 px-2">Your Tasks</h1>
               <div className="bg-cards rounded-[10px] p-[14px] flex justify-between items-center">
                 <div className="flex items-center flex-1 space-x-2">
                   <div className="flex flex-col space-y-1">
-                    <span className="font-semibold">
-                      Join the Telegram Channel
-                    </span>
-
-                    {message && (
-                      <span className="text-[#ea5b48] text-[12px] pr-8">
-                        {message}
-                      </span>
-                    )}
+                    <span className="font-semibold">Join the Telegram Channel</span>
+                    {message && <span className="text-[#ea5b48] text-[12px] pr-8">{message}</span>}
                   </div>
                 </div>
-                <div className="">
+                <div>
                   {!taskCompleted && (
                     <>
                       {showTaskButton && (
@@ -225,9 +213,7 @@ const TaskTelegram = ({ showModal, setShowModal }) => {
                         >
                           <span>Check</span>
                           {counter !== null && (
-                            <span className="text-[#b0b0b0] pointer-events-none select-none">
-                              ing {counter}s
-                            </span>
+                            <span className="text-[#b0b0b0] pointer-events-none select-none"> ing {counter}s</span>
                           )}
                         </button>
                       )}
@@ -243,44 +229,30 @@ const TaskTelegram = ({ showModal, setShowModal }) => {
                   )}
                 </div>
               </div>
-              {taskCompleted ? (
-                <button className="my-6 w-full py-5 px-3 flex items-center rounded-[12px] justify-center text-center text-[20px] font-medium text-[#6a6978] bg-btn2">
-                  Mission Completed
-                </button>
-              ) : (
-                <button
-                  onClick={handleComplete}
-                  disabled={!isVerified}
-                  className={`my-6 w-full py-5 px-3 flex items-center rounded-[12px] justify-center text-center text-[20px] font-medium 
-                    ${!isVerified ? "text-[#6a6978] bg-btn2" : "text-[#f4f4f4] bg-btn"}`}
-                >
-                  Finish Mission
-                </button>
-              )}
+
+              {/* Finish Mission Button */}
+              <button
+                onClick={taskCompleted ? null : handleComplete}
+                disabled={!isVerified}
+                className={`my-6 w-full py-5 px-3 flex items-center rounded-[12px] justify-center text-center text-[20px] font-medium 
+                ${!isVerified ? "text-[#6a6978] bg-btn2" : "text-[#f4f4f4] bg-btn"}`}
+              >
+                {taskCompleted ? "Mission Completed" : "Finish Mission"}
+              </button>
             </div>
 
+            {/* Congratulations Pop-up */}
             {openComplete && (
               <div className="absolute bottom-0 left-0 right-0 h-[76vh] bg-[#1e2340f7] z-[100] rounded-tl-[20px] rounded-tr-[20px] flex justify-center px-4 py-5">
                 <div className="flex flex-col justify-between w-full py-8">
-                  <div className="flex flex-col items-center justify-center w-full">
+                  <div className="flex flex-col items-center">
                     <div className="w-[120px] h-[120px] rounded-[14px] bg-[#252e57] flex items-center justify-center">
                       <img alt="claim" src={claim} className="" />
                     </div>
-                    <h3 className="font-semibold text-[28px] py-4">
-                      Congratulations
-                    </h3>
-                    <p className="pb-6 text-[#9a96a6] text-[16px]">
-                      You have successfully completed the mission
-                    </p>
-
+                    <h3 className="font-semibold text-[28px] py-4">Congratulations</h3>
+                    <p className="pb-6 text-[#9a96a6] text-[16px]">You have successfully completed the mission</p>
                     <div className="flex items-center flex-1 space-x-2">
-                      <div className="">
-                        <img
-                          src={require('../../images/coinsmall.png')}
-                          className="w-[25px]"
-                          alt="Coin Icon"
-                        />
-                      </div>
+                      <img src={require('../../images/coinsmall.png')} className="w-[25px]" alt="Coin Icon" />
                       <div className="font-bold text-[20px]">50 000</div>
                     </div>
                   </div>
