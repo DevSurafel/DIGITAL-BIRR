ad761b713d27521b1da7a19391de1297bee56655
Surafel Diriba <119774330+DevSurafel@users.noreply.github.com>
2024-10-20 05:52:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-// App.js
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MaintenanceWrapper from './components/MaintenanceWrapper';
+
+// Create a simple Home component (you can move this to a separate file later)
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
 function App() {
   return (
     <MaintenanceWrapper isInMaintenance={true}>
-      {/* Your entire app goes here */}
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* Other routes */}
         </Routes>
       </Router>
     </MaintenanceWrapper>
